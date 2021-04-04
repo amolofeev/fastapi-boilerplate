71eda5af99092eca9f83974cb74a13a8316808b4
Alexander Molofeev <alexandr.nino@gmail.com>
2021-04-04 20:49:55 +0300

    merge common handlers in one

--- src/views/common.py
+++ src/views/common.py
@@ -1,22 +1,42 @@
 from aiopg.sa import SAConnection
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
+from pydantic import BaseModel
+from starlette.responses import JSONResponse, Response
 
 from src.dependencies import pg_connection
 
 router = APIRouter()
 
 
-@router.get('/liveness', response_model=dict, tags=['common'])
+class HealthCheckResponse(BaseModel):
+    postgres: bool
+
+
+@router.get('/liveness',
+            response_model=HealthCheckResponse,
+            responses={500: {'model': HealthCheckResponse}},
+            tags=['common'])
+@router.get('/readiness',
+            response_model=HealthCheckResponse,
+            responses={500: {'model': HealthCheckResponse}},
+            tags=['common'])
 async def liveness_handler(
-        conn: SAConnection = Depends(pg_connection)
-) -> dict:
-    await conn.scalar('select True;')
-    return dict(ok=True)
-
-
-@router.get('/readiness', response_model=dict, tags=['common'])
-async def readiness_handler(
-        conn: SAConnection = Depends(pg_connection)
-) -> dict:
-    await conn.scalar('select True;')
-    return dict(ok=True)
+        response: Response,
+        conn: SAConnection = Depends(pg_connection),
+) -> HealthCheckResponse:
+    coroutines = {
+        'postgres': conn.scalar('select True;')
+    }
+    service_status = {}
+    for service, coroutine in coroutines.items():
+        try:
+            await coroutine
+        except: # noqa
+            service_status[service] = False
+        else:
+            service_status[service] = True
+    status_code = 200
+    if not all(service_status.values()):
+        status_code = 500
+    response.status_code = status_code
+    return HealthCheckResponse(**service_status)
